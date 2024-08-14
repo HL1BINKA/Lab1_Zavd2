c2d6bfd23eea3be841a48a2edcfb2dfc12746ad8
HL1BINKA <148037079+HL1BINKA@users.noreply.github.com>
2024-08-14 13:14:16 +0300

    Add files via upload

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,8 +1,8 @@
 a, b = 0, 1
-fib_sequence = []
+fibanachi = []
 for i in range(1, 26):  
-    if i >= 4 and i <= 25:
-        fib_sequence.append(a)
+    if i >= 5 and i <= 25:
+        fibanachi.append(a)
     a, b = b, a + b
-print(fib_sequence)
-print("Count of elements ", len(fib_sequence))
+print(fibanachi)
+print("Count of elements ", len(fibanachi))
